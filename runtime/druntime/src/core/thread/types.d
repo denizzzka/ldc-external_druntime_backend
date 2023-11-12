bf8fdd56572ada8df3f3dbb93f5aa4984bbdb647
Denis Feklushkin <feklushkin.denis@gmail.com>
2023-11-12 20:53:50 +0300

    Latest import external.core.types removed from druntime

--- runtime/druntime/src/core/thread/types.d
+++ runtime/druntime/src/core/thread/types.d
@@ -25,10 +25,56 @@ version (Posix)
     alias ThreadID = pthread_t;
 }
 else version (FreeStanding)
-    public import external.core.types : ThreadID;
+    alias ThreadID = void*;
 
 version (FreeStanding)
-    public import external.core.types : ll_ThreadData;
+{
+    struct ll_ThreadData
+    {
+        import core.sync.event: Event;
+        import core.atomic;
+
+        ThreadID tid;
+        Event joinEvent;
+        private shared size_t joinEventSubscribersNum;
+
+        void initialize() nothrow @nogc
+        {
+            joinEvent.initialize(true, false);
+        }
+
+        auto getSubscribersNum()
+        {
+            return atomicLoad(joinEventSubscribersNum);
+        }
+
+        void deletionLock() @nogc nothrow
+        {
+            joinEventSubscribersNum.atomicOp!"+="(1);
+        }
+
+        void deletionUnlock() @nogc nothrow
+        {
+            joinEventSubscribersNum.atomicOp!"-="(1);
+        }
+    }
+
+    unittest
+    {
+        ll_ThreadData td;
+        td.initialize();
+
+        assert(td.getSubscribersNum() == 0);
+        td.deletionLock();
+        assert(td.getSubscribersNum() == 1);
+        td.deletionLock();
+        assert(td.getSubscribersNum() == 2);
+        td.deletionUnlock();
+        assert(td.getSubscribersNum() == 1);
+        td.deletionUnlock();
+        assert(td.getSubscribersNum() == 0);
+    }
+}
 else
 struct ll_ThreadData
 {
